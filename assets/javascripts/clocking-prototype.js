9a5c3fd5563b516adea4b9fab80516dd4e4dc985
Eric Davis <edavis@littlestreamsoftware.com>
2011-11-18 12:03:26 -0800

    [#6570] Setup JavaScript

--- assets/javascripts/clocking-prototype.js
+++ assets/javascripts/clocking-prototype.js
@@ -1,1 +1,18 @@
+var Clocking = (function() {
 
+  var initialize = function() {
+    $(document).ready(function() {
+      initializeAfterDomLoaded();
+    });
+  };
+
+  var initializeAfterDomLoaded = function() {
+  };
+
+  // Public API
+  return {
+    initialize: initialize
+  }
+})();
+
+Clocking.initialize();
