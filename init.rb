4c76fb4929bea72d09b3fd4f79c479379bb88412
Eric Davis <edavis@littlestreamsoftware.com>
2011-11-18 11:47:58 -0800

    Generate plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :chiliproject_clocking do
+  name 'Chiliproject Clocking plugin'
+  author 'Author name'
+  description 'This is a plugin for ChiliProject'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
