5e3319fa7de460730b0bb9a2d8b8112622d5f9bf
Eric Davis <edavis@littlestreamsoftware.com>
2011-12-14 12:23:37 -0800

    Create the main clocking element in JavaScript

--- assets/javascripts/clocking_tool.js
+++ assets/javascripts/clocking_tool.js
@@ -1,4 +1,10 @@
 // Clocking Tool Application
 function ClockingTool() {
   this.test = 42;
+  this.container = '#clocking-tool';
+}
+ClockingTool.prototype.init = function() {
+}
+ClockingTool.prototype.draw = function() {
+  $('#clocking-tool-template').tmpl().appendTo(this.container);
 }
--- spec/javascripts/clocking_tool_spec.js
+++ spec/javascripts/clocking_tool_spec.js
@@ -3,9 +3,58 @@ describe("ClockingTool", function() {
 
   beforeEach(function() {
     clockingTool = new ClockingTool();
+    loadFixtures('main.html');
+    //    setFixtures(sandbox({id: "clocking-tool"}))
   });
 
   it("should have a var", function() {
     expect(clockingTool.test).toEqual(42);
+  });
+
+  describe("constructor", function() {
+    xit("should allow setting options via a config object");
+  });
+
+  describe("init()", function() {
+    it("should not raise an exception", function() {
+      expect(function() {
+        clockingTool.init();
+      }).not.toThrow("anything");
+    });
+
+    xit("should configure the tool");
+  });
+
+  describe("draw()", function() {
+    beforeEach(function() {
+      clockingTool.init();
+    });
+
+    it("should create a div inside the container", function() {
+      clockingTool.draw();
+
+      expect($('#clocking-tool')).toContain('div.clocking-tool-inner');
+    });
+
+    it("should create the header elements", function() {
+      clockingTool.draw();
+
+      expect($('#clocking-tool')).toContain('.header .message-box');
+      expect($('#clocking-tool')).toContain('.header .popout');
+    });
+
+    it("should create the form section elements", function() {
+      clockingTool.draw();
+
+      expect($('#clocking-tool')).toContain('.form-container');
+    });
+
+    it("should create the recent section elements", function() {
+      clockingTool.draw();
+
+      expect($('#clocking-tool')).toContain('.recent-container');
+    });
+
+    xit("should draw pretty stuff");
   });
 });
