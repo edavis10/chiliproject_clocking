502932604cca156b78818d629af409379940dd94
Eric Davis <edavis@littlestreamsoftware.com>
2011-12-14 12:37:11 -0800

    Stub out the form and some basic styles

--- assets/javascripts/clocking_tool.js
+++ assets/javascripts/clocking_tool.js
@@ -5,6 +5,13 @@ function ClockingTool() {
 }
 ClockingTool.prototype.init = function() {
 }
+// Add some stub data during development
+ClockingTool.prototype.addStubData = function() {
+  $(this.container + " .header .message-box").html("Hi TEST, please clock your time below");
+  $(this.container + " .header .popout").html("[O]");
+  $(this.container + " .form-container form").append("<input>").append("<input type='submit'>");
+}
 ClockingTool.prototype.draw = function() {
   $('#clocking-tool-template').tmpl().appendTo(this.container);
+  this.addStubData();
 }
