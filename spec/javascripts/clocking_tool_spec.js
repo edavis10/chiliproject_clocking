ee5178d119df082947e8722567b80ee41245dce9
Eric Davis <edavis@littlestreamsoftware.com>
2011-12-15 10:12:27 -0800

    Refactor: move test setup to beforeEach()

--- spec/javascripts/clocking_tool_spec.js
+++ spec/javascripts/clocking_tool_spec.js
@@ -18,63 +18,47 @@ describe("ClockingTool", function() {
   });
 
   describe("draw()", function() {
-    it("should create a div inside the container", function() {
+    beforeEach(function() {
       clockingTool.draw();
+    });
 
+    it("should create a div inside the container", function() {
       expect($('#clocking-tool')).toContain('div.clocking-tool-inner');
     });
 
     it("should create the header elements", function() {
-      clockingTool.draw();
-
       expect($('#clocking-tool')).toContain('.header .message-box');
       expect($('#clocking-tool')).toContain('.header .popout');
     });
 
     it("should create the form section elements", function() {
-      clockingTool.draw();
-
       expect($('#clocking-tool')).toContain('.form-container');
     });
 
     it("should create the recent section elements", function() {
-      clockingTool.draw();
-
       expect($('#clocking-tool')).toContain('.recent-container');
     });
 
     it("should populate the form action with the configured url", function() {
-      clockingTool.draw();
-
       expect($('.form-container form')).toHaveAttr("action", "/time_entries.json");
-
     });
 
     it("should populate the field of spent on with today's date (YYYY-mm-dd format)", function() {
       today = new Date();
-      clockingTool.draw();
-
       expect($('.form-container #time_entry_spent_on')).toHaveValue(formatDateToISO(today));
-
     });
 
     it("should add a blank option to the Activity field", function() {
-      clockingTool.draw();
-
       expect($('.form-container #time_entry_activity_id option').length).toEqual(1);
       expect($('.form-container #time_entry_activity_id option:first')).toHaveValue('');
       expect($('.form-container #time_entry_activity_id option:first')).toHaveText('Activity');
     });
 
     it("should populate the message with a welcome message", function() {
-      clockingTool.draw();
-
       expect($('.message-box')).toHaveText('Hi demo user, please clock your time below');
     });
 
     it("should disable the fields", function() {
-      clockingTool.draw();
-
       expect($('#project_id')).toBeDisabled();
       expect($('#issue_search')).toBeDisabled();
       expect($('#time_entry_activity_id')).toBeDisabled();
