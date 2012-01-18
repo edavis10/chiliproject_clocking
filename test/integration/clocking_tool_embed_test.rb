525a7f7af4277834dd33d83bb88735c5f5dee9c1
Eric Davis <edavis@littlestreamsoftware.com>
2012-01-18 11:22:10 -0800

    Move the test.html content into a Rails view

---
+++ test/integration/clocking_tool_embed_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class ClockingToolEmbedTest < ActionController::IntegrationTest
+  def setup
+    Setting.rest_api_enabled = "1"
+    assert Setting.rest_api_enabled?
+    @user = User.generate!(:password => 'test', :password_confirmation => 'test').reload
+    @project = Project.generate!.reload
+    @role = Role.generate!(:permissions => [:view_issues, :log_time])
+    User.add_to_project(@user, @project, @role)
+
+    login_as(@user.login, 'test')
+  end
+
+  context "standalone page" do
+    setup do
+      visit("/clocking_tool")
+    end
+    
+    should "be successful" do
+      assert_response :success
+    end
+    
+    should "include the clocking tool javascript" do
+      puts page.body
+      assert has_css?('script[src*=clocking_tool]') # contains substring match
+    end
+    
+    should "draw the clocking tool" do
+      assert has_css?('script', :text => /draw/)
+    end
+    
+    should "include the base layout's header section to reuse CSS and JavaScript"
+
+  end
+end
