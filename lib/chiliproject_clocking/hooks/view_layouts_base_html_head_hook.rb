6f8192d39019bcf788bc3a0b438572a63f1dfb7c
Eric Davis <edavis@littlestreamsoftware.com>
2012-01-19 11:15:29 -0800

    Only load jquery.js if it isn't being loaded by the app

--- lib/chiliproject_clocking/hooks/view_layouts_base_html_head_hook.rb
+++ lib/chiliproject_clocking/hooks/view_layouts_base_html_head_hook.rb
@@ -2,13 +2,26 @@ module ChiliprojectClocking
   module Hooks
     class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        return javascript_include_tag('underscore-min.js', :plugin => 'chiliproject_clocking') +
-          javascript_include_tag('jquery.js', :plugin => 'chiliproject_clocking') +
-          javascript_include_tag('jquery.tmpl.min.js', :plugin => 'chiliproject_clocking') +
-          javascript_include_tag('helpers.js', :plugin => 'chiliproject_clocking') +
-          javascript_tag('jQuery.noConflict();') +
-          javascript_include_tag('clocking_tool.js', :plugin => 'chiliproject_clocking') +
-          stylesheet_link_tag('clocking_tool', :plugin => 'chiliproject_clocking')
+        js_libs = [javascript_include_tag('underscore-min.js', :plugin => 'chiliproject_clocking')]
+
+        jquery_included = begin
+                            ChiliProject::Compatibility && ChiliProject::Compatibility.using_jquery?
+                          rescue NameError
+                            # No compatibilty test
+                            false
+                          end
+        unless jquery_included
+          js_libs << javascript_include_tag('jquery.js', :plugin => 'chiliproject_clocking')
+          js_libs << javascript_tag('jQuery.noConflict();')
+        end
+
+        js_libs << javascript_include_tag('jquery.tmpl.min.js', :plugin => 'chiliproject_clocking')
+        js_libs << javascript_include_tag('helpers.js', :plugin => 'chiliproject_clocking')
+        js_libs << javascript_include_tag('clocking_tool.js', :plugin => 'chiliproject_clocking')
+
+        js_libs << stylesheet_link_tag('clocking_tool', :plugin => 'chiliproject_clocking')
+
+        return js_libs.join(' ')
 
       end
     end
